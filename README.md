0b1209aac80f769c37763c667c3d39faed55a2d4
Christopher Rokita <chris@livio.io>
2016-09-27 11:30:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Hey. Use these commands to run me. You won't regret it
+
+sdl HMI
+
+master branch:
+
+```docker run -d -p 8080:8080 -e "HMI_WEBSOCKET_ADDR=127.0.0.1:8080" --name hmi crokita/discovery-generic-hmi:master```
---
+++ master/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Changing Flags.js HMI ServerAddress to ${HMI_WEBSOCKET_ADDR}"
+# Replace IP and Port in flags file to match the machine's IP address
+perl -pi -e 's/127.0.0.1:8087/'$HMI_WEBSOCKET_ADDR'/g' /var/www/app/Flags.js
+# Start the node app
+webpack && npm start
