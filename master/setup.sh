8eb26292df920a0adc857d22286b40d61272f851
Christopher Rokita <chris@livio.io>
2016-10-07 12:42:27 -0400

    Change to using nginx to serve html file

--- master/setup.sh
+++ master/setup.sh
@@ -2,5 +2,6 @@
 echo "Changing Flags.js HMI ServerAddress to ${HMI_WEBSOCKET_ADDR}"
 # Replace IP and Port in Controller file with the env variable passed in
 perl -pi -e 's/localhost:8087/'$HMI_WEBSOCKET_ADDR'/g' /usr/app/webapp/src/js/Controllers/Controller.js
-# Start the node app
-webpack && npm start
+# bundle everything with webpack, then run nginx
+webpack
+nginx
